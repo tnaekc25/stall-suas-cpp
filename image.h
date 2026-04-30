1952b24bb9d1cc077be0892153206e77cc632f85
Tuna EKICI <tnaekc2@gmail.com>
2026-04-30 17:54:57 +0300

    Resolution changed

--- image.h
+++ image.h
@@ -3,13 +3,13 @@
 
 #include <opencv2/opencv.hpp>
 
-#define IMAGE_WIDTH 1920 /* In pixels */
-#define IMAGE_HEIGHT 1080 /* In pixels */
+#define IMAGE_WIDTH 1280 /* In pixels */
+#define IMAGE_HEIGHT 720 /* In pixels */
 
 #define CAM_FX 800.0
 #define CAM_FY 800.0
 #define CAM_PITCH 0 /* In radians */
 
-#define CAM_FPS 60
+#define CAM_FPS 30
 
 #endif
